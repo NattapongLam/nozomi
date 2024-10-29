96fad37f2f6051e21a784d0ad3d07d49552ccf87
NattapongLam <it.9nat@gmail.com>
2024-10-29 15:19:26 +0700

    ok

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,15 +7,15 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
         <meta content="Themesbrand" name="author" />
-        <link rel="shortcut icon" href="{{URL::asset('assets/images/favicon.ico')}}">
-        <link href="{{URL::asset('assets/css/bootstrap.min.css')}}" id="bootstrap-style" rel="stylesheet" type="text/css" />
-        <link href="{{URL::asset('assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
-        <link href="{{URL::asset('assets/css/app.min.css')}}" id="app-style" rel="stylesheet" type="text/css" />
+        <link rel="shortcut icon" href="{{asset('assets/images/favicon.ico')}}">
+        <link href="{{asset('assets/css/bootstrap.min.css')}}" id="bootstrap-style" rel="stylesheet" type="text/css" />
+        <link href="{{asset('assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
+        <link href="{{asset('assets/css/app.min.css')}}" id="app-style" rel="stylesheet" type="text/css" />
         <link rel="stylesheet" type="text/css" href="{{ asset('assets/libs/toastr/toastr.min.css') }}">
-        <link href="{{URL::asset('assets/libs/select2/css/select2.min.css')}}" rel="stylesheet" type="text/css" />
-        <link href="{{URL::asset('assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css')}}" rel="stylesheet" type="text/css" />
-        <link href="{{URL::asset('assets/libs/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css')}}" rel="stylesheet" type="text/css" />
-        <link href="{{URL::asset('assets/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css')}}" rel="stylesheet" type="text/css" />   
+        <link href="{{asset('assets/libs/select2/css/select2.min.css')}}" rel="stylesheet" type="text/css" />
+        <link href="{{asset('assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css')}}" rel="stylesheet" type="text/css" />
+        <link href="{{asset('assets/libs/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css')}}" rel="stylesheet" type="text/css" />
+        <link href="{{asset('assets/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css')}}" rel="stylesheet" type="text/css" />   
         <!-- font-->
         <link href='https://fonts.googleapis.com/css?family=Kanit' rel='stylesheet'>
         <style>
@@ -49,26 +49,26 @@
         <!-- Right bar overlay-->
         <div class="rightbar-overlay"></div>       
         <!-- JAVASCRIPT -->
-        <script src="{{URL::asset('assets/libs/jquery/jquery.min.js')}}"></script>
-        <script src="{{URL::asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-        <script src="{{URL::asset('assets/libs/metismenu/metisMenu.min.js')}}"></script>
-        <script src="{{URL::asset('assets/libs/simplebar/simplebar.min.js')}}"></script>
-        <script src="{{URL::asset('assets/libs/node-waves/waves.min.js')}}"></script>
-        <script src="{{URL::asset('assets/js/app.js')}}"></script>
-        <script src="{{URL::asset('assets/libs/select2/js/select2.min.js')}}"></script>
-        <script src="{{URL::asset('assets/libs/datatables.net/js/jquery.dataTables.min.js')}}"></script>
-        <script src="{{URL::asset('assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
-        <script src="{{URL::asset('assets/libs/datatables.net-buttons/js/dataTables.buttons.min.js')}}"></script>
-        <script src="{{URL::asset('assets/libs/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js')}}"></script>
-        <script src="{{URL::asset('assets/libs/jszip/jszip.min.js')}}"></script>
-        <script src="{{URL::asset('assets/libs/pdfmake/build/pdfmake.min.js')}}"></script>
-        <script src="{{URL::asset('assets/libs/pdfmake/build/vfs_fonts.js')}}"></script>
-        <script src="{{URL::asset('assets/libs/datatables.net-buttons/js/buttons.html5.min.js')}}"></script>
-        <script src="{{URL::asset('assets/libs/datatables.net-buttons/js/buttons.print.min.js')}}"></script>
-        <script src="{{URL::asset('assets/libs/datatables.net-buttons/js/buttons.colVis.min.js')}}"></script>
-        <script src="{{URL::asset('assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js')}}"></script>
-        <script src="{{URL::asset('assets/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js')}}"></script>
-        <script src="{{URL::asset('assets/js/pages/datatables.init.js')}}"></script>     
+        <script src="{{asset('assets/libs/jquery/jquery.min.js')}}"></script>
+        <script src="{{asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+        <script src="{{asset('assets/libs/metismenu/metisMenu.min.js')}}"></script>
+        <script src="{{asset('assets/libs/simplebar/simplebar.min.js')}}"></script>
+        <script src="{{asset('assets/libs/node-waves/waves.min.js')}}"></script>
+        <script src="{{asset('assets/js/app.js')}}"></script>
+        <script src="{{asset('assets/libs/select2/js/select2.min.js')}}"></script>
+        <script src="{{asset('assets/libs/datatables.net/js/jquery.dataTables.min.js')}}"></script>
+        <script src="{{asset('assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
+        <script src="{{asset('assets/libs/datatables.net-buttons/js/dataTables.buttons.min.js')}}"></script>
+        <script src="{{asset('assets/libs/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js')}}"></script>
+        <script src="{{asset('assets/libs/jszip/jszip.min.js')}}"></script>
+        <script src="{{asset('assets/libs/pdfmake/build/pdfmake.min.js')}}"></script>
+        <script src="{{asset('assets/libs/pdfmake/build/vfs_fonts.js')}}"></script>
+        <script src="{{asset('assets/libs/datatables.net-buttons/js/buttons.html5.min.js')}}"></script>
+        <script src="{{asset('assets/libs/datatables.net-buttons/js/buttons.print.min.js')}}"></script>
+        <script src="{{asset('assets/libs/datatables.net-buttons/js/buttons.colVis.min.js')}}"></script>
+        <script src="{{asset('assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js')}}"></script>
+        <script src="{{asset('assets/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js')}}"></script>
+        <script src="{{asset('assets/js/pages/datatables.init.js')}}"></script>     
         <!-- toastr plugin -->
         <script src="{{ asset('/assets/libs/toastr/toastr.min.js') }}"></script>
         <!-- toastr init -->
